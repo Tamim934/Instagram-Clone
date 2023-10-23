b23f204051407d3162a3abf468576ebfe9a8d94d
AUK8 <aukmsi34@gmail.com>
2023-10-23 18:14:40 +0300

    Responsive

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -79,7 +79,7 @@ export default function CustomizedMenus() {
 <div className='hidden dark:block'>
 <svg aria-label="Настройки" class="x1lliihq x1n2onr6" color="rgb(255, 255, 255)" fill="rgb(255, 255, 255)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Настройки</title><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="3" x2="21" y1="4" y2="4"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="3" x2="21" y1="12" y2="12"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="3" x2="21" y1="20" y2="20"></line></svg>
 </div>
-<span className='text-black dark:text-white text-[16px]'>Ещё</span>
+<span className='text-black dark:text-white text-[16px] hidden xl:block'>Ещё</span>
 </div>
 </div>
       </Button>
