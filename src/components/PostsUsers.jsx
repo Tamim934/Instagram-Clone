a760e26541af0f4d35c5ed14737f69331a56e555
Tamim934 <140682649+Tamim934@users.noreply.github.com>
2023-10-24 23:13:19 +0500

    profile post fixed

--- src/components/PostsUsers.jsx
+++ src/components/PostsUsers.jsx
@@ -1,26 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { axiosRequest, getToken } from '../utilities/axiosRequest';
 
-
-
-const Post = ({ username, caption, likes, comments }) => {
-  const userId = getToken()?.sid;
-  const [newimage,setnewImage]=useState()
-  const getProfile=async()=>{
-    try {
-      const {data}= await axiosRequest.get(`Post/get-posts/${userId}`)
-console.log(data?.data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  useEffect(() => {
-    getProfile();
-  }, []);
-
-
-
+const Post = ({ post }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const imageUrl = `http://65.108.148.136:8085/images/${post.images[0]}`; 
 
   return (
     <div 
@@ -28,47 +11,46 @@ console.log(data?.data)
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >                                          
-      {/* <img  src={image} alt="Post" className=" w-96  h-96 max4:w-80 max4:h-80 max600:w-60 max600:h-60 max6:w-40 max6:h-40  object-cover" /> */}
-      {/* {isHovered && (
+      <img src={imageUrl} alt="Post" className="w-96 h-96 max4:w-80 max4:h-80 max600:w-60 max600:h-60 max6:w-40 max6:h-40 object-cover" />
+      {isHovered && (
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center gap-[30px] justify-center p-4">
-          <p className="text-white flex items-center gap-[5px]"> 🤍{likes}</p>
-          <p className="text-white  flex items-center gap-[5px]">💬 {comments}</p>
+          <p className="text-white flex items-center gap-[5px]"> 🤍{post.postLikeCount}</p>
+          <p className="text-white flex items-center gap-[5px]">💬 {post.commentCount}</p>
         </div>
-      )} */}
+      )}
     </div>
   );
 };
-  
 
 
 const Posts = () => {
- 
+  const [posts, setPosts] = useState([]);
+  const userId = getToken()?.sid;
 
+  const getPosts = async () => {
+    try {
+      const { data } = await axiosRequest.get(`Post/get-posts?UserId=${userId}`);
+      setPosts(data.data);
+      console.log(data.data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const posts = [
-    { username: 'user1', image: '/src/assets/imagesuserprofile/OIP (1).jpeg', caption: 'Caption 1', likes: 100, comments: 20 },
-    { username: 'user2', image: '/src/assets/imagesuserprofile/uwp3981822 (1).jpeg', caption: 'Caption 2', likes: 200, comments: 40 },
-    { username: 'user1', image: '/src/assets/imagesuserprofile/wp4717570-ronaldo-hd-4k-mobile-wallpapers (1).jpg', caption: 'Caption 1', likes: 100, comments: 20 },
-    { username: 'user2', image: '/src/assets/imagesuserprofile/uwp3981824 (2).jpeg', caption: 'Caption 2', likes: 200, comments: 40 },
-  
-    // more posts...
-  ];
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
-<div className="grid  gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-3">
-     {posts.map((post, index) => (
-       <Post 
-         key={index}
-         username={post.username}
-         image={post.image}
-         caption={post.caption}
-         likes={post.likes}
-         comments={post.comments}
-       />
-     ))}
-   </div>
+    <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-3">
+      {posts.map((post, index) => (
+        <Post key={index} post={post}
+         />
+      ))}
+    </div>
   );
 };
 
 export default Posts;
+
 
